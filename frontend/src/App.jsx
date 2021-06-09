1856bc6c408701fc5f06a1c3420e6e9424307ddb
danieltrusca <truscadani@yahoo.com>
2021-06-09 15:47:59 +0300

    adding react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,18 @@
-import React, {useState} from "react";
-import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/Intro"
-import Portfolio from "./components/portfolio/Portfolio"
-import Works from "./components/works/Works"
-import Testimonials from "./components/testimonials/Testimonials"
-import Contact from "./components/contact/Contact";
-import Menu from "./components/menu/Menu";
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import Home from "./pages/Home";
+
 
 import "./App.scss";
 
 const App=()=>{
-    const [menuOpen,setMenuOpen] = useState(false)
+    
     return (
-        <div className="app">
-            <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-            <div className="sections">
-                <Intro/>
-                <Portfolio/>
-                <Works/>
-                <Testimonials/>
-                <Contact/>
-            </div>
-        </div>
+        <>
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </>
     )
 }
 
